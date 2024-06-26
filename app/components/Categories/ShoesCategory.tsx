132054b38bf32c4ed1eb8bdf7258f809a4a47d25
MishSoft <michaelmakaveli123@gmail.com>
2024-06-26 21:59:04 +0400

    Change categories images

--- app/components/Categories/ShoesCategory.tsx
+++ app/components/Categories/ShoesCategory.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
+
 export default function ShoesCategory() {
   return (
-    <div className=" w-[100%] sm:w-[80%] md:w-[50%] xl:w-[45%] sm:rounded-[20px] bg-purple-100">
-      <div className="flex flex-col">
-        <div className="flex items-center justify-center">
-          <div className="flex flex-col items-center  gap-5">
-            <h2 className="md:text-[25px] text-[18px] font-bold">Shoes</h2>
-            <button className="sm:pl-[15px] sm:pr-[15px] sm:pt-[10px] sm:gap-5 sm:pb-[10px] flex ml-[10px] items-center border border-purple-300 pl-[10px] pr-[10px] pt-[5px] pb-[5px] rounded-[50px]">
-              Shop Now
-              <FaArrowRight />
+    <div className="w-[100%] sm:w-[80%] md:w-[50%] xl:w-[45%] sm:rounded-[20px] border border-purple-300 shadow-lg transition-transform duration-200 hover:shadow-xl hover:-translate-y-2">
+      <div className="flex flex-col p-5">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col items-start gap-5">
+            <h2 className="md:text-[25px] text-[18px] font-bold text-gray-800">
+              Shoes
+            </h2>
+            <button className="sm:px-[15px] sm:py-[10px] flex items-center gap-2 border border-purple-300 px-[10px] py-[5px] rounded-full transition-colors duration-200 hover:bg-purple-300 hover:text-white">
+              Shop Now <FaArrowRight />
             </button>
           </div>
           <img
-            className="w-[70%] sm:w-[60%] md:w-[50%]"
+            className="w-[20%] sm:w-[30%] md:w-[20%] "
             src="/shoes.png"
-            alt=""
+            alt="Shoes"
           />
         </div>
       </div>
